52471f9780af21d730ed61f269647ec12f2953a8
Quinten Yearsley <qyearsley@gmail.com>
2016-05-23 20:26:03 -0700

    A change that I don't remember.

--- calc.h
+++ calc.h
@@ -2,6 +2,20 @@
 #define UNSET -1
 
 int SYMBOL_TABLE[TABLE_SIZE];
+
 int lookup(int symbol_num);
 void set(int symbol_num, int value);
 void reset_table();
+
+typedef struct {
+  int value;
+  int symbol_num;
+  int op;
+  Expr *left;
+  Expr *right;
+} Expr;
+
+Expr *Expr_new(int value, int symbol_num, int op, Expr *left, Expr *right) {
+  struct Expr *e = malloc(sizeof(struct Expr));
+
+}
